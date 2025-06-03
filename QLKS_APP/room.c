2dcfde1e95dbe05f80ccb063a0483b9a22df05ef
rettn <97347572+rettn@users.noreply.github.com>
2025-06-03 20:49:16 +0700

    Update room.c

--- QLKS_APP/room.c
+++ QLKS_APP/room.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 #include "room.h"
-
-void inputRoom(Room* r) {
+#include "color_utils.h"
+void inputRoom(Room *r) {
     printf("Nhap ma phong: ");
-    readLine(r->roomNo, sizeof(r->roomNo));
-
+    fgets(r->roomNo, sizeof(r->roomNo), stdin);
+    r->roomNo[strcspn(r->roomNo, "\n")] = 0;
     printf("Nhap ten phong: ");
-    readLine(r->roomName, sizeof(r->roomName));
-
+    fgets(r->roomName, sizeof(r->roomName), stdin);
+    r->roomName[strcspn(r->roomName, "\n")] = 0;
     printf("Nhap tang: ");
-    scanf("%d", &r->floor);
-    getchar(); 
-
+    while (scanf("%d", &r->floor) != 1) {
+        printError("Loi nhap tang! Nhap lai: ");
+        while (getchar() != '\n');
+    }
+    while (getchar() != '\n');
     printf("Nhap so nguoi toi da: ");
-    scanf("%d", &r->numMax);
-    getchar();
-
+    while (scanf("%d", &r->numMax) != 1) {
+        printError("Loi nhap so nguoi! Nhap lai: ");
+        while (getchar() != '\n');
+    }
+    while (getchar() != '\n');
     printf("Nhap gia: ");
-    scanf("%d", &r->price);
-    getchar();
+    while (scanf("%d", &r->price) != 1) {
+        printError("Loi nhap gia! Nhap lai: ");
+        while (getchar() != '\n');
+    }
+    while (getchar() != '\n');
 }
-
-void displayRoom(const Room* r) {
-    printf("Ma phong: %s, Ten phong: %s, Tang: %d, So nguoi toi da: %d, Gia tien: %d\n",
-        r->roomNo, r->roomName, r->floor, r->numMax, r->price);
+void displayRoom(const Room *r) {
+    printf("| %-10s | %-15s | Tang: %-2d | So nguoi: %-2d | Gia: %-7d |\n",
+           r->roomNo, r->roomName, r->floor, r->numMax, r->price);
 }
