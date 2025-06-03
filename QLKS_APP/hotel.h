ecea4ced376f76c1338c40d71a25909d81f819d2
rettn <97347572+rettn@users.noreply.github.com>
2025-06-03 20:48:02 +0700

    Update hotel.h

--- QLKS_APP/hotel.h
+++ QLKS_APP/hotel.h
@@ -1,11 +1,8 @@
 #ifndef HOTEL_H
 #define HOTEL_H
-
 #include "room.h"
 #include "booking.h"
-
 #define MAX_ROOMS 50
-
 typedef struct {
     char hotelNo[20];
     char name[50];
@@ -14,11 +11,6 @@ typedef struct {
     Room roomList[MAX_ROOMS];
     int roomCount;
 } Hotel;
-
 void inputHotel(Hotel *h);
-void displayHotel(const Hotel *h);
-void displayAllHotel(const Hotel *h);
-int calculateProfit(const Hotel *h, const Booking *bookings, int bookingCount);
 void showRoomAvailable(const Hotel *h, const Booking *bookings, int bookingCount);
-
 #endif
