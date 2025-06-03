47f1ea14b6c0962a8ffc336e862a2f7ae043075d
rettn <97347572+rettn@users.noreply.github.com>
2025-06-03 20:59:48 +0700

    Create color_utils.h

---
+++ QLKS_APP/color_utils.h
@@ -0,0 +1,9 @@
+#ifndef COLOR_UTILS_H
+#define COLOR_UTILS_H
+#include <windows.h>
+#include <stdio.h>
+  void setColor(int color);
+  void resetColor();
+  void printSuccess(const char* msg);
+v  oid printError(const char* msg);
+#endif
