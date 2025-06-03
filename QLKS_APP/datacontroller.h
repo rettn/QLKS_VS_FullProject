427456178a555513afcb8d59d541672473912d19
rettn <97347572+rettn@users.noreply.github.com>
2025-06-03 20:44:57 +0700

    Update datacontroller.h

--- QLKS_APP/datacontroller.h
+++ QLKS_APP/datacontroller.h
@@ -1,27 +1,19 @@
 #ifndef DATACONTROLLER_H
 #define DATACONTROLLER_H
-
 #include "customer.h"
 #include "hotel.h"
 #include "booking.h"
-
 #define MAX_CUSTOMERS 100
 #define MAX_HOTELS 50
 #define MAX_BOOKINGS 200
-
 typedef struct {
     Customer customerList[MAX_CUSTOMERS];
     int customerCount;
-
     Hotel hotelList[MAX_HOTELS];
     int hotelCount;
-
     Booking bookingList[MAX_BOOKINGS];
     int bookingCount;
 } DataController;
-
 extern DataController dataController;
-
 Hotel* findHotel(const char *hotelNo);
-
 #endif
