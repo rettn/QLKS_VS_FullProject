f2d569846757833e8530d73d6bf8f6e33cf06aa1
rettn <97347572+rettn@users.noreply.github.com>
2025-06-03 20:42:51 +0700

    Update customer.c

--- QLKS_APP/customer.c
+++ QLKS_APP/customer.c
@@ -1,31 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include "customer.h"
-
+#include "utility.h"
+#include "color_utils.h"
 void inputCustomer(Customer *c) {
     printf("Nhap CCCD: ");
     readLine(c->cmtnd, sizeof(c->cmtnd));
-    
     inputCustomerWithoutCMTND(c);
 }
-
-
-void inputCustomerWithoutCMTND(Customer* c) {
+void inputCustomerWithoutCMTND(Customer *c) {
     printf("Nhap ten: ");
     readLine(c->fullname, sizeof(c->fullname));
-
     c->age = readInt("Nhap tuoi: ");
-
     printf("Nhap gioi tinh: ");
     readLine(c->gender, sizeof(c->gender));
-
     printf("Nhap dia chi: ");
     readLine(c->address, sizeof(c->address));
 }
-
-
-
 void displayCustomer(const Customer *c) {
-    printf("Ten: %s, Tuoi: %d, CMTND: %s, Gioi tinh: %s, Dia chi: %s\n",
+    printf("Ten: %s | Tuoi: %d | CMTND: %s | Gioi tinh: %s | Dia chi: %s\n",
            c->fullname, c->age, c->cmtnd, c->gender, c->address);
 }
