fdd19bb03eea6c9850d88e1e76a08f0f0c9a9f39
rettn <97347572+rettn@users.noreply.github.com>
2025-06-03 20:51:08 +0700

    Update utility.h

--- QLKS_APP/utility.h
+++ QLKS_APP/utility.h
@@ -1,16 +1,7 @@
 #ifndef UTILITY_H
 #define UTILITY_H
-
-#include <time.h>
 #include <stdio.h>
 #include <string.h>
-
-int convertStringToDateTime(const char *datetimeStr, struct tm *result);
-void convertDateTimeToString(const struct tm *t, char *buffer, int bufferSize);
-
-
-
-void readLine(char* buffer, int size);
-int readInt(const char* prompt);
+int readInt(const char *prompt);
+void readLine(char *buffer, int size);
 #endif
-
