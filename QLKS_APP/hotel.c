e96a93e8dbb3c3253c61619ed84d70ac78276259
rettn <97347572+rettn@users.noreply.github.com>
2025-06-03 20:45:47 +0700

    Update hotel.c

--- QLKS_APP/hotel.c
+++ QLKS_APP/hotel.c
@@ -1,60 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 #include "hotel.h"
-
-void inputHotel(Hotel* h) {
+#include "utility.h"
+#include "color_utils.h"
+void inputHotel(Hotel *h) {
     printf("Nhap ma khach san: ");
     readLine(h->hotelNo, sizeof(h->hotelNo));
-
+    
     printf("Nhap ten khach san: ");
     readLine(h->name, sizeof(h->name));
-
+    
     printf("Nhap dia chi: ");
     readLine(h->address, sizeof(h->address));
-
+    
     printf("Nhap loai: ");
     readLine(h->type, sizeof(h->type));
-
+    
     char temp[10];
     printf("Nhap so phong: ");
     readLine(temp, sizeof(temp));
     h->roomCount = atoi(temp);
-
+    
     for (int i = 0; i < h->roomCount; i++) {
         printf("--- Phong %d ---\n", i + 1);
         inputRoom(&h->roomList[i]);
     }
 }
-
-
-void displayHotel(const Hotel *h) {
-    printf("Ten KS: %s, Ma KS: %s, Dia chi: %s, Loai: %s\n",
-        h->name, h->hotelNo, h->address, h->type);
-}
-
-void displayAllHotel(const Hotel *h) {
-    displayHotel(h);
-    printf("Danh sach phong:\n");
-    for (int i = 0; i < h->roomCount; i++) {
-        displayRoom(&h->roomList[i]);
-    }
-}
-
-int calculateProfit(const Hotel *h, const Booking *bookings, int bookingCount) {
-    int profit = 0;
-    for (int i = 0; i < bookingCount; i++) {
-        if (strcmp(bookings[i].hotelNo, h->hotelNo) == 0) {
-            for (int j = 0; j < h->roomCount; j++) {
-                if (strcmp(bookings[i].roomNo, h->roomList[j].roomNo) == 0) {
-                    profit += h->roomList[j].price;
-                    break;
-                }
-            }
-        }
-    }
-    return profit;
-}
-
 int isRoomAvailable(const Hotel *h, const Booking *bookings, int bookingCount, const Room *room) {
     for (int i = 0; i < bookingCount; i++) {
         if (strcmp(bookings[i].hotelNo, h->hotelNo) == 0 &&
@@ -64,10 +35,13 @@ int isRoomAvailable(const Hotel *h, const Booking *bookings, int bookingCount, c
     }
     return 1;
 }
-
 void showRoomAvailable(const Hotel *h, const Booking *bookings, int bookingCount) {
-    displayHotel(h);
-    printf("Danh sach phong trong:\n");
+    setColor(14);
+    printf("Khach san: %s | Ma: %s | Dia chi: %s | Loai: %s\n",
+           h->name, h->hotelNo, h->address, h->type);
+    resetColor();
+    printf("| %-10s | %-15s | Tang | So nguoi | Gia       |\n", "Ma phong", "Ten phong");
+    printf("----------------------------------------------------------\n");
     for (int i = 0; i < h->roomCount; i++) {
         if (isRoomAvailable(h, bookings, bookingCount, &h->roomList[i])) {
             displayRoom(&h->roomList[i]);
