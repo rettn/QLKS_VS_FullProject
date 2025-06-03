c2617491763fa1b2fea0afc488945fe05984a847
rettn <97347572+rettn@users.noreply.github.com>
2025-06-03 20:58:26 +0700

    Create color_utils.c

---
+++ QLKS_APP/color_utils.c
@@ -0,0 +1,17 @@
+#include "color_utils.h"
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+void resetColor() {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+}
+void printSuccess(const char* msg) {
+    setColor(10);
+    printf("✔ %s\n", msg);
+    resetColor();
+}
+void printError(const char* msg) {
+    setColor(12);
+    printf("✘ %s\n", msg);
+    resetColor();
+}
