808a4c2e9b6cfe0d2e8bc5a10506cf0bae1396dd
rettn <97347572+rettn@users.noreply.github.com>
2025-06-03 20:47:03 +0700

    Update hotel.c

--- QLKS_APP/hotel.c
+++ QLKS_APP/hotel.c
@@ -6,21 +6,16 @@
 void inputHotel(Hotel *h) {
     printf("Nhap ma khach san: ");
     readLine(h->hotelNo, sizeof(h->hotelNo));
-    
     printf("Nhap ten khach san: ");
     readLine(h->name, sizeof(h->name));
-    
     printf("Nhap dia chi: ");
     readLine(h->address, sizeof(h->address));
-    
     printf("Nhap loai: ");
     readLine(h->type, sizeof(h->type));
-    
     char temp[10];
     printf("Nhap so phong: ");
     readLine(temp, sizeof(temp));
     h->roomCount = atoi(temp);
-    
     for (int i = 0; i < h->roomCount; i++) {
         printf("--- Phong %d ---\n", i + 1);
         inputRoom(&h->roomList[i]);
